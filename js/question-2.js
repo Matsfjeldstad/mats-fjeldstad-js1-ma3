f63f09b2444626e072ec69b83fbce4a6d6660fb8
Matsfjeldstad <89415897+Matsfjeldstad@users.noreply.github.com>
2022-02-13 19:59:26 +0100

    update

--- js/question-2.js
+++ js/question-2.js
@@ -1,39 +1,28 @@
-// Question 1
-// Convert the function below to an arrow function:
-// function getRemainder(a,b) {
-// return a % b;
-// }
-// Question 2
+const topGames = document.querySelector('.top-games');
 
-const getRemainder = (a, b) => {
-  return a % b;
-};
-
-const topGames = document.querySelector(".top-games");
-const documentDody = document.querySelector("body");
-
-const Rawg_API = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=07d8164699e544d697fb867d719fa22d`;
+const Rawg_API = `https://api.rawg.io/api/games?dates=2021-01-01,2021-12-31&ordering=-rating&key=07d8164699e544d697fb867d719fa22d`;
 (async function getApi() {
   try {
     const response = await fetch(Rawg_API);
     const rawgData = await response.json();
     const gameResults = rawgData.results;
     topGames.innerHTML = ``;
-    topGames.classList.remove("centered");
-    topGames.classList.add("game-list");
-    console.log(gameResults);
+    topGames.classList.remove('centered');
+    topGames.classList.add('game-list');
     for (let i = 0; i < gameResults.length; i++) {
       if (i === 8) {
         break;
       }
       console.log(gameResults[i].name);
       topGames.innerHTML += `
-      <div class="game-container">
+      <div class="game-container"  style="background-image: url('${gameResults[i].background_image}');">
       <div class="game-name"> ${gameResults[i].name}</div>
       <div class="game-rating"><i class="fa-solid fa-star"></i>${gameResults[i].rating}</div>
       <div class ="game-tags-number">Tags (${gameResults[i].tags.length})</div>
       </div>
       `;
     }
-  } catch {}
+  } catch (error) {
+    topGames.innerHTML = `<div class="error">Oh no something wrong happend! ${error}</div>}`;
+  }
 })();
